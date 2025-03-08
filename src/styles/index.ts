2aaf7d23c4499038e8529a6ca293f8614d0f10f4
Jake Caughern <80003881+jcaughern@users.noreply.github.com>
2025-03-08 12:04:08 -0600

    updated to react 19

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,4 @@
-export { default as contentStyle } from "./contentStyle";
-export { default as headingStyle } from "./headingStyle";
-export { default as logoStyle } from "./logoStyle";
-export { default as navStyle } from "./navStyle";
+export { default as contentStyle } from './contentStyle';
+export { default as headingStyle } from './headingStyle';
+export { default as logoStyle } from './logoStyle';
+export { default as navStyle } from './navStyle';
